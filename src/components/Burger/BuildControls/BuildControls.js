d4a6372081c9a2086ba6bf62368e9a706e8b698c
Ben Piotti <benpiotti@Bens-MBP.home>
2018-07-30 12:28:04 -0400

    added redux

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,10 +4,10 @@ import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'},
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
@@ -15,15 +15,15 @@ const buildControls = (props) => (
         <p>Current Price: <strong> {props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl
-                key={ctrl.label} 
+                key={ctrl.label}
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
                 removed={() => props.ingredientRemoved(ctrl.type)}
-                disabled={props.disabled[ctrl.type]}/>
+                disabled={props.disabled[ctrl.type]} />
         ))}
         <button className={classes.OrderButton}
-        disabled={!props.purchaseable}
-        onClick={props.ordered}>ORDER NOW</button>
+            disabled={!props.purchaseable}
+            onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
 
